07dbc51c778e2964bf4407610dc46c39fa93fbc7
William Morgan <williammorgan864@gmail.com>
2023-05-02 22:04:57 +0100

    added variables for global game data

--- resourses/scripts/game.js
+++ resourses/scripts/game.js
@@ -1,5 +1,13 @@
 // define objects for global game data
 
+var gameData; // global for translation data
+
+const gameSessionData = {
+    lang:"",
+    currentPos:0,
+}
+
+
 // add event listener for selecting languge 
 
 // event listener for submiting answer 
