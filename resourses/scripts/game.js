4e22bdba91922ead1f79ab7bd1e155bea70d82da
William Morgan <williammorgan864@gmail.com>
2023-05-02 22:39:43 +0100

    added handle submit functionality

--- resourses/scripts/game.js
+++ resourses/scripts/game.js
@@ -15,6 +15,7 @@ langSelect.addEventListener("submit", handleLangSubmit, false);
 var card = document.getElementById("flip-card")
 card.addEventListener("click", handleCardClick)
 
+document.getElementById("ans-submit").addEventListener("submit", handleAnswerSubmit, false)
 
 function handleLangSubmit(event){
     event.preventDefault();
@@ -44,9 +45,47 @@ setCardVal = (text) => {
     document.getElementById("card-text").innerHTML = text;
 }
 
-
 function handleCardClick(event){
     let currentpair = gameData[gameSessionData.currentPos];
     setCardVal(currentpair["translation"]);
 }
 
+function handleAnswerSubmit(event){
+    event.preventDefault();
+    let ans = document.getElementById('answer').value;
+    let currentpair = gameData[gameSessionData.currentPos];
+    // check if ans close or one of accepted
+    if(ansAccepted(ans, currentpair["translation"])){
+        //play some kind of win anim and go to next pair 
+        goToNextPair();
+        showWinAnim();
+    }
+
+    else{
+        //prompt user to guess again 
+        showLoseAnim();
+    }
+}
+
+ansAccepted = (ans, corrAns) => {
+    ans = ans.toLowerCase();
+    corrAns = corrAns.toLowerCase();
+    let ansArray = corrAns.split(',')
+  
+    return ansArray.includes(ans);
+}
+
+goToNextPair = () => {
+    gameSessionData.currentPos+=1;
+    let currentpair = gameData[gameSessionData.currentPos];
+    setCardVal(currentpair["original"]);
+
+}
+
+showWinAnim = () => {
+    document.getElementById('answer').style.background = 'green';
+}
+
+showLoseAnim = () => {
+    document.getElementById('answer').style.background = 'red';
+}
