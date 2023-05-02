7c963fd62c9d7ecd65ea9265f850f6e6acdf375f
William Morgan <williammorgan864@gmail.com>
2023-05-02 22:21:07 +0100

    lang-submit event handler implemented

--- resourses/scripts/game.js
+++ resourses/scripts/game.js
@@ -9,6 +9,38 @@ const gameSessionData = {
 
 
 // add event listener for selecting languge 
+var langSelect = document.getElementById("lang-select");
+langSelect.addEventListener("submit", handleLangSubmit, false); 
+
+
+function handleLangSubmit(event){
+    event.preventDefault();
+    let lang = document.getElementById("lang").value;
+    getJsonFromFile("resourses/data/"+lang+'.json');
+
+    setLang(lang);
+    hideLangSelect();
+}
+
+async function getJsonFromFile(file) {
+    let response = await fetch(file);
+    gameData = await response.json();
+    // must find a way to avoid calling set card val from getText
+    setCardVal(gameData[0]["original"]);
+  }
+
+  setLang = (l) => {
+    gameSessionData.lang = l;
+}
+
+hideLangSelect = () =>{
+    document.getElementById("lang-popup").style.display = 'none';  
+}
+
+setCardVal = (text) => {
+    document.getElementById("card-text").innerHTML = text;
+}
+
 
 // event listener for submiting answer 
 
